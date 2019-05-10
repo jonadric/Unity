ebf62ec5833c3198f92b6f715fbc9d1b79275c63
Jonnathan Astroz <jonathanastroz@gmail.com>
2019-05-09 19:36:31 -0500

    pruebas

--- Bola/Assets/Scripts/CameraController.cs
+++ Bola/Assets/Scripts/CameraController.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Transform target;
     public GameObject player;
     private Vector3 offset;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
         offset = transform.position - player.transform.position;
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        float moveHorizontal = Input.GetAxis("Horizontal");
         transform.position = player.transform.position + offset;
-        
+        transform.LookAt(target);
+        transform.Rotate(0.0f, moveHorizontal, 0.0f);
     }
 }
